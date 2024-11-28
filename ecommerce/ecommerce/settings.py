5c9e89bc9d49a2929ef021be0a29bbc62dca5815
Pablo Fernández <91889823+Letee2@users.noreply.github.com>
2024-11-28 22:28:36 +0100

    Update settings.py for production in render

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--g_3z%)r5+-el!55aw6b-=hwn%(@y-dfp$u0-ug=4j(1dy@h_&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
